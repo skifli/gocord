4b967f3da3020cdb30af814f8253a6497963b47d
skifli <121291719+skifli@users.noreply.github.com>
2023-07-23 19:46:01 +0100

    feat: redo testing mechanism for more updates

--- gocord_test.go
+++ gocord_test.go
@@ -2,6 +2,7 @@ package gocord
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/skifli/gocord/api"
@@ -14,8 +15,11 @@ func check(err error) {
 }
 
 func TestMain(t *testing.T) {
+	data, err := os.ReadFile("_token")
+	check(err)
+
 	client := &Client{
-		SelfBot: SelfBotToken(""),
+		SelfBot: SelfBotToken(string(data)),
 	}
 
 	check(client.Init())
